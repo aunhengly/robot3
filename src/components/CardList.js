4cedf93309b6aa847b22b3b63146129a8d13a27c
Hengly Aun <aunhengly@gmail.com>
2022-04-24 13:55:18 -0700

    add deploy script

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import Card from './Card';
 
+//The same as wrap in the return below:
+// const CardArray = robots.map((user,i) => {
+// 		return (
+// 			<Card
+// 				key={i}
+// 				id={robots[i].id}
+// 				name={robots[i].name}
+// 				email={robots[i].email}
+// 			/>
+// 		);
+// 	})
+// 	return (
+// 		<div>
+// 				{CardArray}
+// 		</div>
+// 	);
+// }
+
 const CardList = ({robots}) => {
-	const CardArray = robots.map((user,i) => {
-		return (
-			<Card
-				key={i}
-				id={robots[i].id}
-				name={robots[i].name}
-				email={robots[i].email}
-			/>
-		);
-	})
+	// if(true){
+	// 	throw new Error ('Noooooo! Errors Erros');
+	// }
 	return (
 		<div>
-				{CardArray}
+				{
+					robots.map((user,i) => {
+						return (
+							<Card
+								key={i}
+								id={robots[i].id}
+								name={robots[i].name}
+								email={robots[i].email}
+							/>
+						);
+					})
+				}
 		</div>
 	);
 }
