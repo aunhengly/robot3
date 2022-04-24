7266982f9499383c96aa7303eb2f6936676e0536
Hengly Aun <aunhengly@gmail.com>
2022-04-24 15:16:43 -0700

    Updates

---
+++ precache-manifest.4552ead02644a9ace4224693f218388b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6468bbd6667c5642abb",
+    "url": "/robot3/static/css/main.07cc9e69.chunk.css"
+  },
+  {
+    "revision": "d6468bbd6667c5642abb",
+    "url": "/robot3/static/js/main.177b1b80.chunk.js"
+  },
+  {
+    "revision": "4c9f6c2911fffa0a6d6a",
+    "url": "/robot3/static/js/runtime~main.a84d57f7.js"
+  },
+  {
+    "revision": "e9db3a74a4cbb3629da1",
+    "url": "/robot3/static/css/2.34654669.chunk.css"
+  },
+  {
+    "revision": "e9db3a74a4cbb3629da1",
+    "url": "/robot3/static/js/2.10279204.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robot3/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot3/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b2741bd81bff30645cdf410dfc425118",
+    "url": "/robot3/index.html"
+  }
+];
